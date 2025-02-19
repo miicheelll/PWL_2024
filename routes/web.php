380aaa99bbb7d9c5fb91d435f865e66da369dea2
Michelle Dorani Shiba <doranimichelle@gmail.com>
2025-02-19 09:14:52 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+   
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Michelle Dorani Shiba (2341720113)';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+    
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+    
