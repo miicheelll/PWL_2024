ded914eacb4fc70ff837ff9bc69842be4ad3a805
Michelle Dorani Shiba <doranimichelle@gmail.com>
2025-02-19 10:35:48 +0700

    Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,6 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
    
+Route::get('/greeting', [WelcomeController::class,'greeting']);
+
+    
